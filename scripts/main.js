63dba8acd6266a443e6d4befc87e1785011d4f7f
nasanos <19861991+nasanos@users.noreply.github.com>
2017-10-09 18:26:15 -0500

    0.4

--- scripts/main.js
+++ scripts/main.js
@@ -7,15 +7,6 @@ $(document).ready(function(){
   });
   $(".button-collapse").sideNav("show");
 
-  //Menu Setup
-  var usrlist = {{usrlist|safe}}
-  for (var usr in usrlist) {
-    if (usrlist[usr] != "{{username}}") {
-      $("#slide-out").append("<li><a href='#' class='black-text sidemenuopt'>"+usrlist[usr]+"</a></li>");
-    };
-  };
-  $("#slide-out").append("<nav id='navbottom' class='white center'><div class='nav-wrapper container'><div class='row'><a href='{{url_for("logoff")}}' class='btn right offset-s10 z-depth-1 blue darken-1 waves-effect waves-light'>Logoff</a></div></div></nav>");
-
   //Setup Socket.IO
   namespace = "/msgs";
   var socket = io.connect(location.protocol+"//"+document.domain+":"+location.port+namespace);
